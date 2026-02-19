fe2de777ee2c3b6edaad07124c8400330403dbbe
archi0911 <archiprajapati09@gmail.com>
2026-02-19 16:57:43 +0530

    Added the JS file

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
-function addStudent() {
-    return "Add function";
+let students = [];
+
+function addStudent(fname, age, grade) {
+  let student = {
+    id: Math.floor(Math.random() * 100),
+    name: fname,
+    age: age,
+    grade: grade
+  }
+  students.push(student);
+  console.log(student);
 }
+
 function listStudent() {
-    return "List function";
+  console.log(students);
 }
-function searchStudent() {
-    return "Search function";
+
+function searchStudent(name) {
+  let len = students.length;
+  let i;
+  for(i=0; i<len; i++) {
+    if (name == students[i].name) {
+      console.log(students[i]);
+      return;
+    }
+  }
+  console.log("Name not found");
 }
-function updateStudent() {
-    return "Update function";
+
+function updateStudent(id, fname, age, grade) {
+  // students[0].name = fname;
+  let len = students.length;
+  let i;
+  for(i=0; i<len; i++) {
+    if (id == students[i].id) {
+      students[i].name = fname;
+      students[i].age = age;
+      students[i].grade = grade;
+      console.log(students[i]);
+
+      console.log("Student Successfully Updated")
+      return;
+    }
+  }
+  console.log("Student not Found");
 }
-function removeStudent() {
-    return "Remove function";
+
+function removeStudent(id) {
+  students.filter(item => item.id !== id);
+  console.log(students);
 }
 
 console.log("Student Management App Loaded!");
-console.log("Available Functions:")
+console.log("Available Functions:");
+console.log("addStudent(name, age, grade)");
+console.log("listStudent()");
+console.log("searchStudent(name)");
+console.log("updateStudent(id)");
+console.log("removeStudent()");
 
-const details = ['Archi', 20, 'A'];
-console.log(details);
-
-let add = addStudent();
-console.log(add);
-
-let ls = listStudent();
-console.log(ls);
-
-let search = searchStudent();
-console.log(search);
-
-let update = updateStudent();
-console.log(update);
-
-let rm = removeStudent();
-console.log(rm);
